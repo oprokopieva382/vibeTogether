26905231b35ef4fe12be33301e9eb5ff5d0e9867
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-10-10 12:49:04 -0500

    finish onSubmitArtistBio func that takes the input value chack if valid and run getArtistBio func for get response

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
-import {getArtistBio, getEventData} from "./restAPI.js"
+import { getArtistBio, getEventData } from "./restAPI.js";
 
+const willUpdateFromInput = document.getElementById("willUpdateFromInput");
+const willUpdateIdFormButton = document.getElementById(
+  "willUpdateIdFormButton"
+);
+const willUpdateErrorForm = document.getElementById("willUpdateErrorForm");
 
+const queryParams = ["artistName=Drake"];
 
+const onSubmitArtistBio = (e) => {
+  e.preventDefault();
 
+  const name = willUpdateFromInput.value.trim();
 
+  if (name === "") {
+    willUpdateErrorForm.textContent = "Please enter a valid name";
+  } else {
+    willUpdateErrorForm.textContent = "";
+    //getArtistBio(name)
+  }
+};
 
-const queryParams=["artistName=Drake"]
-
-
-//getEventData(queryParams)
+willUpdateIdFormButton.addEventListener("click", onSubmitArtistBio);
+//getEventData(queryParams)//here temporary, in use now for see data response in console
 //getArtistBio("Drake");//here temporary, in use now for see data response in console
