1fdc06abf12c0e71e9a54394c154a1b3daa28a34
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-10-11 01:36:59 -0400

    created form with artist name, radio buttons and dropdown for genre

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 import {getArtistBio} from "./restAPI.js"
 
 
-
+var eventsBtn = document.querySelector("#eventsBtn");
+var eventsModal = document.querySelector("#events-modal");
+var eventsModalBg = document.querySelector("#events-modal-bg");
 
 
 
 
 
 //getArtistBio("Drake");//here temporary, in use now for see data response in console
+
+
+
+
+
+// Event listeners
+
+eventsBtn.addEventListener("click", function(event) {
+    eventsModal.classList.add("is-active");
+});
+
+eventsModalBg.addEventListener("click", function(event) {
+    eventsModal.classList.remove("is-active");
+})
